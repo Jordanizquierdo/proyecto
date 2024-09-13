6018b948d44c86af12f027490bd93fcece4e761c
Jordanizquierdo <169169895+Jordanizquierdo@users.noreply.github.com>
2024-09-13 11:37:14 -0300

    .

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'app1/index.html')
+
+def serie1(request):
+    datos = {"nombre":"stranger things","descripcion":"una serie buena","valoracion":"7/10"}
+    return render(request, 'app1/pag1.html',datos)
